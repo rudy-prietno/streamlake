f080163a3b1abe98962b2631ba4217cec663832b
rudy-prietno <prietnorudy@gmail.com>
2025-09-20 10:11:49 +0700

    Update admin-manage-replication-slot.sql

--- rds-postgresql/admin-manage-replication-slot.sql
+++ rds-postgresql/admin-manage-replication-slot.sql
@@ -43,3 +43,59 @@ FROM pg_create_logical_replication_slot('cdc_databasename_slot', 'pgoutput');
 -- ##################################
 SELECT pg_drop_replication_slot('cdc_databasename_slot');
 
+
+-- ##################################
+
+-- The unique name of the replication slot.
+-- The output plugin used for logical replication.
+    -- Common values:
+    -- pgoutput → built-in logical replication (standard).
+
+-- The database where the slot was created.
+-- true if a client (replica or CDC tool) is currently connected and consuming the slot.
+-- Process ID of the active connection using this slot.
+-- The WAL (log sequence number) position that must be kept to support this slot.
+-- The WAL position confirmed as processed by the client.
+
+-- ##################################
+SELECT
+  slot_name,
+  plugin,
+  database,
+  active,
+  active_pid,
+  restart_lsn,
+  confirmed_flush_lsn
+FROM pg_replication_slots
+
+
+-- ##################################
+
+-- A publication defines what data changes leave the database.
+-- This setup is required for CDC pipelines to capture inserts, updates, and deletes.
+    -- Fine-grained control means:
+        -- Only business-critical tables are replicated.
+        -- We can avoid unnecessary storage and cost by excluding irrelevant tables.
+
+-- cdc_pub_database_prod → the identifier for this publication.
+-- public.table_name (listed multiple times here, but typically you would include one or more distinct tables).
+-- (publish = 'insert, update, delete') → the operations that will be replicated.
+    -- Options include:
+        -- insert → new rows.
+        -- update → modified rows.
+        -- delete → removed rows.
+
+
+-- ##################################
+CREATE PUBLICATION cdc_pub_database_prod
+FOR TABLE
+    public.table_name,
+    public.table_name
+WITH (publish = 'insert, update, delete');
+
+
+-- ##################################
+
+
+
+-- ##################################
